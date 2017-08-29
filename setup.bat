1b485a301e5196dfef7c595d368e26b2e22112fc
lucentminds <scott@lifecorp.com>
2017-08-29 16:19:57 -0500

    Added more details for building and running.

--- setup.bat
+++ setup.bat
@@ -1,4 +1,10 @@
 @echo off
-set GOROOT=C:\go\386
+
+rem 32 bit setup.
+set GOROOT=C:\go\1.9\386
+
+rem 64 bit setup.
+rem set GOROOT=C:\go\1.9\amd64
+
 set GOPATH=%~dp0
 set PATH=%PATH%;%GOROOT%\bin
